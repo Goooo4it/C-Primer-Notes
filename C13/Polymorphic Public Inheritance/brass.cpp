736e83c8034c78f98d3072f63925981b6b3a06ec
Zhi <zhijiang@umich.edu>
2023-12-18 15:54:29 -0500

    Polymorphism exmaple

---
+++ C13/Polymorphic Public Inheritance/brass.cpp
@@ -0,0 +1,10 @@
+// bank account class methods
+
+#include <iostream>
+#include "brass.h"
+using std::cout;
+using std::endl;
+using std::string;
+
+//formating stuff   
+typedef std::ios_base::fmtflags format;
